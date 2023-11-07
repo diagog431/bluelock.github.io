8b7939fd466c0ff8f33acb86e06eef81c9a531c5
diagog431 <148229641+diagog431@users.noreply.github.com>
2023-11-07 09:36:36 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let likes_total = 0;
 function like(){
     likes_total++;
     const like = document.getElementById("likes_show");
-    like.innerHtml = "Likes: "+likes_total;
+    like.innerText = "Likes: "+likes_total;
     console.log(like)
     
 }
